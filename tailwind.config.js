ddeef2e26fd47aa214f9235d80fc57255f76131d
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2024-02-22 18:02:29 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'D-DIN-Medium': ['D-DIN-Medium', 'sans-serif'],
+        'D-DIN-Light': ['D-DIN-Light', 'sans-serif'],
+        'D-DIN-Bold': ['D-DIN-Bold', 'sans-serif'],
+        'D-DIN-Regular': ['D-DIN-Regular', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
